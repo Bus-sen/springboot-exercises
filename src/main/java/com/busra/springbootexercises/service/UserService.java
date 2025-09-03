ef8913d36fcd4b9f2a6df4d4f0cb32e00ab6cec8
Bus-sen <snbusra66@gmail.com>
2025-09-03 19:47:30 +0300

    [SP-1] - service interface

--- src/main/java/com/busra/springbootexercises/service/UserService.java
+++ src/main/java/com/busra/springbootexercises/service/UserService.java
@@ -1,40 +1,11 @@
 package com.busra.springbootexercises.service;
 
-import com.busra.springbootexercises.dto.request.UserRequest;
-import com.busra.springbootexercises.dto.response.UserResponse;
 import com.busra.springbootexercises.model.User;
-import com.busra.springbootexercises.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository userRepository;
-
-    public UserResponse create(UserRequest userRequest) {
-        User user = new User();
-        user.setUsername(userRequest.getUsername());
-        user.setPassword(userRequest.getPassword());
-        userRepository.save(user);
-
-        UserResponse userResponse = new UserResponse();
-        userResponse.setMessage("Kullanıcı oluşturuldu");
-        return userResponse;
-    }
-
-    public List<UserResponse> findAllUsers(){
-        List<User> users = userRepository.findAll();
-        List<UserResponse> userResponseList = new ArrayList<>();
-        for(User user : users){
-            UserResponse userResponse = new UserResponse();
-            userResponse.setMessage("Kullanıcı Bilgileri: " + user.getUsername() + " | " + user.getEmail());
-            userResponseList.add(userResponse);
-        }
-        return userResponseList;
-    }
+public interface UserService {
+    User create(User user);
+    List<User> getAllUsers();
+    User update(long id, User user);
+    void delete(long id);
 }
